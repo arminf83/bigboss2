436f4a894c79126fdc3a4817d403ebf2c5a2fcf4
Armin <armin@armin.linux>
2024-09-18 14:30:59 +0330

    armin

--- home/urls.py
+++ home/urls.py
@@ -1,10 +1,11 @@
 
 from django.urls import path , include
 
-from .views import Home
+from .views import home
 
 urlpatterns = [
-    path('', Home.as_view() , name='home'),
+    # path('', Home.as_view() , name='home'),    #class base
+    path('',home,name='home')
 
 
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,16 @@
+# class base
+# from django.shortcuts import render
+# from django.views.generic import ListView
+# from product.models import Product
+
+# class Home(ListView):
+#     model = Product
+#     template_name = 'home/base.html'
+#     context_object_name = 'products'
+
 from django.shortcuts import render
 from django.views.generic import ListView
 from product.models import Product
 
-class Home(ListView):
-    model = Product
-    template_name = 'home/base.html'
-    context_object_name = 'products'
-
+def home (request):
+    return render (request,'home/home.html')
